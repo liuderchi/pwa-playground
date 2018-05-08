bc58862e6858a9fcebe9548a5e18a2406835a381
Te-Chi Liu <liuderchi@gmail.com>
2018-05-08 21:36:32 +0800

    introduce prettier + geoip service

--- main.js
+++ main.js
@@ -1,92 +1,87 @@
-
 // Progressive Enhancement
 if (navigator.serviceWorker) {
+  // Register SW
+  navigator.serviceWorker.register('sw.js').catch(console.error);
 
-    // Register SW
-    navigator.serviceWorker.register('sw.js').catch(console.error);
-
-    // Giphy cache clean
-    function giphyCacheClean(giphys) {
-
-        // Get service worker registration
-        navigator.serviceWorker.getRegistration().then(function(reg){
-
-            // Only post message to active SW
-            if( reg.active ) reg.active.postMessage({ action: 'cleanGiphyCache', giphys:giphys });
-        });
-    }
+  // Giphy cache clean
+  function giphyCacheClean(giphys) {
+    // Get service worker registration
+    navigator.serviceWorker.getRegistration().then(function(reg) {
+      // Only post message to active SW
+      if (reg.active)
+        reg.active.postMessage({ action: 'cleanGiphyCache', giphys: giphys });
+    });
+  }
 }
 
-// Giphy API object
-var giphy = {
-    url: 'https://api.giphy.com/v1/gifs/trending',
-    query: {
-        api_key: '54452c59b31e4d14aca213ec76014baa',
-        limit: 12
-    }
+const geoipAPI = {
+  url: 'http://api.ipstack.com/124.11.64.18',
+  query: {
+    access_key: 'e22e57b562e511b646985a6d79af1344'
+  }
 };
 
-const CORS_ANYWHERE_DOMAIN = 'https://cors-anywhere.herokuapp.com'
+const CORS_ANYWHERE_DOMAIN = 'https://cors-anywhere.herokuapp.com';
 
-var darkSkyAPI = {
-  url: 'https://api.darksky.net/forecast/501c815982d69b200afd4b1671befbd8/37.8267,-122.4233'
+const darkSkyAPI = {
+  url: 'https://api.darksky.net/forecast/501c815982d69b200afd4b1671befbd8'
+};
+
+function f2c(f) {
+  return Math.ceil((f - 32) * 5 / 9 * 100) / 100;
 }
 
 // Update trending giphys
 function update() {
+  // Toggle refresh state
+  $('#update .icon').toggleClass('d-none');
 
-    // Toggle refresh state
-   $('#update .icon').toggleClass('d-none');
+  $.get(geoipAPI.url, geoipAPI.query).done(res => {
+    console.warn(`fetching weather data in ${res.region_name}...`);
 
-    // Call Giphy API
     // NOTE add Allow all origins in reponse by hitting cors-anywhere proxy
-    $.get(`${CORS_ANYWHERE_DOMAIN}/${darkSkyAPI.url}`)
+    $.get(
+      `${CORS_ANYWHERE_DOMAIN}/${darkSkyAPI.url}/${res.latitude},${
+        res.longitude
+      }`
+    )
+      .done(function(res) {
+        $('#giphys').empty();
+        var latestGiphys = [];
 
-        // Success
-        .done( function (res) {
+        console.warn(
+          res.daily.data.map(({ apparentTemperatureMax }) =>
+            f2c(apparentTemperatureMax)
+          )
+        );
+        console.warn(res.daily.data.map(({ humidity }) => humidity));
+        console.warn(res.daily.data.map(({ pressure }) => pressure));
 
-            // Empty Element
-            $('#giphys').empty();
+        // TODO append temp graph
+        // TODO append humidity graph
+        // TODO append pressure graph
 
-            // Populate array of latest Giphys
-            var latestGiphys = [];
+        // Inform the SW (if available) of current Giphys
+        if (navigator.serviceWorker) giphyCacheClean(latestGiphys);
+      })
 
-            console.warn({ temp: res.currently.temperature, daily: res.daily.data});
+      // Failure
+      .fail(function() {
+        $('.alert').slideDown();
+        setTimeout(function() {
+          $('.alert').slideUp();
+        }, 2000);
+      })
 
-            // Loop Giphys
-            $.each( res.daily.data, (i, data) => {
+      // Complete
+      .always(function() {
+        // Re-Toggle refresh state
+        $('#update .icon').toggleClass('d-none');
+      });
+  });
 
-                // Add to latest Giphys
-                // latestGiphys.push( giphy.images.downsized_large.url );
-
-                // Add Giphy HTML
-                $('#giphys').prepend(
-                  `<div class="col-sm-6 col-md-4 col-lg-3 p-1">
-                    ${data.temperatureHigh}, ${data.temperatureLow}
-                  </div>`
-                );
-            });
-
-            // Inform the SW (if available) of current Giphys
-            if( navigator.serviceWorker ) giphyCacheClean(latestGiphys);
-        })
-
-        // Failure
-        .fail(function(){
-
-            $('.alert').slideDown();
-            setTimeout( function() { $('.alert').slideUp() }, 2000);
-        })
-
-        // Complete
-        .always(function() {
-
-            // Re-Toggle refresh state
-            $('#update .icon').toggleClass('d-none');
-        });
-
-    // Prevent submission if originates from click
-    return false;
+  // Prevent submission if originates from click
+  return false;
 }
 
 // Manual refresh
