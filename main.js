9db7444f185cf5ee1d55eb58442b683083385e34
Te-Chi Liu <liuderchi@gmail.com>
2018-05-13 00:54:30 +0800

    chart style config

--- main.js
+++ main.js
@@ -32,12 +32,31 @@ function f2c(f) {
   return Math.ceil((f - 32) * 5 / 9 * 10) / 10;
 }
 
-function drawChart({ id, data, label }) {
+function drawChart({
+  id,
+  data,
+  borderColor = 'rgba(243, 154, 30, 1)',
+  backgroundColor = 'rgba(243, 154, 30, 0.2)',
+  title,
+  yTick = v => v,
+}) {
+  Chart.defaults.global.defaultFontSize = 14;
   const weekDays = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
     today = new Date().getDay(),
-    backgroundColor = ['rgba(243, 154, 30, 0.2)'],
-    borderColor = ['rgba(243, 154, 30, 1)'],
-    borderWidth = 1;
+    options = {
+      title: {
+        display: true,
+        text: title,
+        fontSize: 14,
+        fontStyle: 'default',
+      },
+      legend: {
+        display: false,
+      },
+      scales: {
+        yAxes: [{ ticks: { callback: yTick } }],
+      },
+    }
 
   const ctx = document.getElementById(id).getContext('2d');
   new Chart(ctx, {
@@ -46,14 +65,14 @@ function drawChart({ id, data, label }) {
       labels: [...weekDays.slice(today), ...weekDays.slice(0, today)],
       datasets: [
         {
-          label,
           data,
-          backgroundColor,
-          borderColor,
-          borderWidth
+          backgroundColor: [backgroundColor],
+          borderColor: [borderColor],
+          borderWidth: 1,
         }
       ]
-    }
+    },
+    options,
   });
 }
 
@@ -89,20 +108,27 @@ function update() {
           data: weatherData.daily.data.map(({ apparentTemperatureMax }) =>
             f2c(apparentTemperatureMax)
           ),
-          label: 'Temperature (Celsius)'
-          // NOTE do not draw label
+          title: 'Temperature',
+          borderColor: 'rgb(40, 167, 69, 1)',
+          backgroundColor: 'rgb(40, 167, 69, 0.2)',
+          yTick: v => `${v} \u00B0C`,
         });
         drawChart({
           id: 'precipProbability',
           data: weatherData.daily.data.map(
             ({ precipProbability }) => precipProbability
           ),
-          label: 'Precipitation Probability'
+          title: 'Precipitation Probability',
+          borderColor: 'rgb(23, 162, 184, 1)',
+          backgroundColor: 'rgb(23, 162, 184, 0.2)',
+          yTick: v => `${v*100} %`,
         });
         drawChart({
           id: 'uvIndex',
           data: weatherData.daily.data.map(({ uvIndex }) => uvIndex),
-          label: 'UV Index'
+          title: 'UV Index',
+          borderColor: 'rgb(102, 16, 242, 1)',
+          backgroundColor: 'rgb(102, 16, 242, 0.2)',
         });
 
         latestWeatherData.push(weatherData)
